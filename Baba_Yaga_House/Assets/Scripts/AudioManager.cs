88509af3c9f849502f19193117e83415e9d83a90
AndyCubico <99950309+AndyCubico@users.noreply.github.com>
2024-05-01 18:53:01 +0200

    Audio manager working
    Trabajamos en la escena Sara

--- Baba_Yaga_House/Assets/Scripts/AudioManager.cs
+++ Baba_Yaga_House/Assets/Scripts/AudioManager.cs
@@ -9,12 +9,46 @@ public class AudioManager : MonoBehaviour
     public AudioClip[] grassAudios;
     public AudioClip[] indoorAudios;
 
+    public float cooldown = 2.0f;
+    private float _timer = 0;
+    private Rigidbody _rigidbody;
+    private AudioSource source;
+
+    void Awake()
+    {
+        // Get the rigidbody on this.
+        _rigidbody = GetComponent<Rigidbody>();
+        source = GetComponent<AudioSource>();
+    }
+
     void PlayAudioRandom()
     {
-        AudioSource source = GetComponent<AudioSource>();
         source.volume = Random.Range(0.9f, 1.0f);
         source.pitch = Random.Range(0.8f, 1.2f);
         source.PlayOneShot(currentAudios[Random.Range(0, currentAudios.Length)]);
+    }
+
+    void AudioTimer()
+    {
+        if (Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Vertical") > 0)
+        {
+            if (_timer >= cooldown)
+            {
+                PlayAudioRandom();
+                _timer = 0;
+            }
+
+            else
+            {
+                _timer += Time.deltaTime;
+            }
+        }
+
+        else
+        {
+            source.Stop();
+            _timer = 0;
+        }
     }
 
     private void OnCollisionStay(Collision collision)
@@ -26,18 +60,21 @@ public class AudioManager : MonoBehaviour
                 {
                     Debug.Log("Dock");
                     currentAudios = dockAudios;
+                    AudioTimer();
                     break;
                 }
             case "Grass":
                 {
                     Debug.Log("Grass");
                     currentAudios = grassAudios;
+                    AudioTimer();
                     break;
                 }
             case "Indoor":
                 {
                     Debug.Log("Indoor");
                     currentAudios = indoorAudios;
+                    AudioTimer();
                     break;
                 }
             default:
@@ -45,5 +82,6 @@ public class AudioManager : MonoBehaviour
                     break;
                 }
         }
+
     }
 }
